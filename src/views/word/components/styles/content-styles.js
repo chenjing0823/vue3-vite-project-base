222640ca3c374d135b4ad1295a03de3aaba74c4f
陈靖 <jing.chen@xbongbong.com>
2024-03-06 15:37:15 +0800

    feat: 之前改的自定义样式，提到自定义文件内

--- src/views/word/components/styles/content-styles.js
+++ src/views/word/components/styles/content-styles.js
@@ -1,5 +1,7 @@
-const styles = `p {margin: 5px 0;}
+const styles = `html{background-color:#f7f8fa;height:100%;display: flex;justify-content: center;}
+  p {margin: 5px 0;}
   img {max-width:100%;height:auto}
-  .mce-content-body:not([dir=rtl])[data-mce-placeholder]:not(.mce-visualblocks)::before {left:unset;top:78px;}`
+  .mce-content-body:not([dir=rtl])[data-mce-placeholder]:not(.mce-visualblocks)::before {left:unset;top:78px;}
+  .mce-content-body{width:782px;background: #fff;padding: 72px;min-height: 100%;height: max-content;}`
 
 export default styles
